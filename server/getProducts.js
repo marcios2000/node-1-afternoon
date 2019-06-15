fd4007de21fb428e0de3ddab25a76a2a651ccd14
marcios2000 <marciotexas@yahoo.com>
2019-06-15 15:33:29 -0500

    commit

--- server/getProducts.js
+++ server/getProducts.js
@@ -7,7 +7,7 @@ const products = require('../products.json')
 //     if (!item) {
 //         return response.status(500).send("item not in list")
 //     }
-//     response.status(200).send(item)
+//     response.status(202).send(item)
 // }
 
 
